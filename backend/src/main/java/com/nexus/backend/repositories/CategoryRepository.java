fbefe13409f83b9cb6214fd090d8607629b09a8b
harshiniakshaya <harshiniakshaya@gmail.com>
2026-09-05 01:00:44 +0530

    feat(backend): integrate Spring Data JPA repository for Category management

---
+++ backend/src/main/java/com/nexus/backend/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.nexus.backend.repositories;
+
+import com.nexus.backend.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
--- backend/src/main/java/com/nexus/backend/service/CategoryServiceImpl.java
+++ backend/src/main/java/com/nexus/backend/service/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.nexus.backend.service;
 
 import com.nexus.backend.model.Category;
+import com.nexus.backend.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -11,47 +13,40 @@ import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
-    private List<Category> categories = new ArrayList<>();
-    private Long nextId = 1L;
+    // private List<Category> categories = new ArrayList<>();
+    // private Long nextId = 1L;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
-        categories.add(category);
+        // category.setCategoryId(nextId++);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
-        Category category = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst()
+        Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found!"));
 
-        categories.remove(category);
+        categoryRepository.delete(category);
         return "Category with category ID:" +
                 categoryId + " deleted!";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        Optional<Category> optionalCategory = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst();
-        if (optionalCategory.isPresent()){
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
-        }
-        else {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND,
-                    "Resouce not found!"
-            );
-        }
+        Category savedCategory = categoryRepository.findById(categoryId).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found"));
+
+        category.setCategoryId(categoryId);
+        savedCategory = categoryRepository.save(category);
+        return savedCategory;
     }
 }
