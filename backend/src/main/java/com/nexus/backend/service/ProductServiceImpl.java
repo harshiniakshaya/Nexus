916cf584cff95cd09761b884a1339fac27aee1c4
harshiniakshaya <harshiniakshaya@gmail.com>
2026-09-25 23:24:09 +0530

    feat(product): implement API endpoint for updating product details

--- backend/src/main/java/com/nexus/backend/service/ProductServiceImpl.java
+++ backend/src/main/java/com/nexus/backend/service/ProductServiceImpl.java
@@ -77,4 +77,24 @@ public class ProductServiceImpl implements ProductService{
         productResponse.setContent(productDTOS);
         return productResponse;
     }
+
+    @Override
+    public ProductDTO updateProduct(Long productId, Product product) {
+        // Get the existing product from DB
+        Product productFromDb = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
+
+        // Update the product info with the one in request body
+        productFromDb.setProductName(product.getProductName());
+        productFromDb.setDescription(product.getDescription());
+        productFromDb.setQuantity(product.getQuantity());
+        productFromDb.setDiscount(product.getDiscount());
+        productFromDb.setPrice(product.getPrice());
+        productFromDb.setSpecialPrice(product.getSpecialPrice());
+
+        // Save the database
+        Product savedProduct = productRepository.save(productFromDb);
+
+        return modelMapper.map(savedProduct, ProductDTO.class);
+    }
 }
