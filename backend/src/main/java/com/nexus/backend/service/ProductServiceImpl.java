bc339284509c4aaa2cd86b9300fe6519674c71b0
harshiniakshaya <harshiniakshaya@gmail.com>
2026-09-25 23:42:22 +0530

    feat(product): implement API endpoint for deleting products

--- backend/src/main/java/com/nexus/backend/service/ProductServiceImpl.java
+++ backend/src/main/java/com/nexus/backend/service/ProductServiceImpl.java
@@ -97,4 +97,13 @@ public class ProductServiceImpl implements ProductService{
 
         return modelMapper.map(savedProduct, ProductDTO.class);
     }
+
+    @Override
+    public ProductDTO deleteProduct(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "ProductId", productId));
+
+        productRepository.delete(product);
+        return modelMapper.map(product, ProductDTO.class);
+    }
 }
